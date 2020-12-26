fixes minor error in td calculation

agent.py
@@ -54,7 +54,7 @@ class TDA2CLearner:
     def calculate_gradients(self, actor_probs, actions, critic_vals, rewards, norm_returns):
         actor_losses, critic_losses = [], []
         for e, probs, action, value, reward, disc_return in enumerate(zip(actor_probs, actions, critic_vals, rewards, norm_returns)):
-            td_advantage = reward + critic_vals[e+1] - value.item()
+            td_advantage = reward + critic_vals[e+1].item() - value.item()
             distribution = Normal(probs)
             actor_losses.append(-distribution.log_prob(action) * td_advantage)
             critic_losses.append(functional.smooth_l1_loss(value, torch.tensor([disc_return])))